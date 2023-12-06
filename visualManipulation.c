42272085a4c4525cf9f3c3ddd9f5e6aeedee89ec
Geraldo Filho <geraldinhofilho14@gmail.com>
2023-12-06 13:31:48 -0300

    Correção em nome com acentuação na interface

--- visualManipulation.c
+++ visualManipulation.c
@@ -1,10 +1,13 @@
 #include "visualManipulation.h"
 #include "memoryAllocation.h"
+#include "tableManipulation.h"
+#include "utils.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <stdbool.h>
 
+//INICIO DAS FUNÇÕES
 void tabelaSelecionada(Tabela *tabela) {
     //ITERAVEIS
     int i;
@@ -15,24 +18,28 @@ void tabelaSelecionada(Tabela *tabela) {
 
     if (tabela != NULL) {
         strncpy(nometabela, tabela->nome, sizeof(nometabela) - 1);
-		nometabela[sizeof(nometabela) - 1] = '\0'; // Garante que a string seja terminada corretamente
+        nometabela[sizeof(nometabela) - 1] = '\0'; // Garante que a string seja terminada corretamente
 
         nometabela[99] = '\0'; // Garante que a string seja terminada corretamente
         
         tamanhoNome = strlen(nometabela); // Calcula o tamanho da string
         
         if(tamanhoNome > 21){
-        	printf("| 9. Sair                               |   ");
-        	for(i = 0;i < 21; i++){
-        		printf("%c",nometabela[i]);
-        	}
-        	printf("...  |\n");
+            printf("| 9. Sair                               |   ");
+            for(i = 0;i < 21; i++){
+                printf("%c",nometabela[i]);
+            }
+            printf("...  |\n");
         }else{
-        	printf("| 9. Sair                               |   %s", nometabela);
-        	for(i = 0; i < quantidadeDeEspacos - tamanhoNome; i++){
-        		printf(" ");
-        	}
-        	printf("|\n");
+            //CALCULA TAMBÉM PARA CASO O NOME TENHA ACENTUAÇÃO
+            int tamanho = strlen(nometabela), diferenca = 0;
+            char sup;
+            for (int i = 0; i < tamanho; i++)
+            {
+                sup = remove_acento(nometabela[i], &diferenca);//CALCULA QUANTOS LETRAS COM ACENTUAÇÃO TEM
+            }
+            printf("| 9. Sair                               |   %-*s", quantidadeDeEspacos-diferenca/2, nometabela);
+            printf("|\n");
         }
     } else {
         printf("| 9. Sair                               |   Sem Tabela Carregada      |\n");
@@ -71,10 +78,10 @@ if(*quadrodeavisos->quadroAvisosAtivo == 0){
             printf(" Tabela:                     |\n");
         }
         if(tabela != NULL){
-        	if(*quadrodeavisos->tabelaCarregada){
-            	printf(" Tabela:                     |\n");
-        	}
-    	}
+            if(*quadrodeavisos->tabelaCarregada){
+                printf(" Tabela:                     |\n");
+            }
+        }
         if(*quadrodeavisos->invalido){
             printf(" Opção inválida!             |\n");
         }
@@ -100,20 +107,29 @@ if(*quadrodeavisos->quadroAvisosAtivo == 0){
         }
         if(*quadrodeavisos->tabelaCarregada){
             if(tabela != NULL){
-            	if(tamanhoNome > 21){
-                	printf(" ");
-                	for(i = 0;i < 21; i++){
-                    	printf("%c",nometabela[i]);
-                	}
-                	printf("...    |\n");
-            	}else{
-                	printf(" %s",nometabela);
-                	for(i = 0; i < quantidadeDeEspacos - tamanhoNome; i++){
-                    	printf(" ");
-                	}
-                	printf("  |\n");
-            	}
-        	}
+                if(tamanhoNome > 21)
+                {
+                    printf(" ");
+                    int tamanho = strlen(nometabela), diferenca = 0;
+                    char sup;
+                    for (int i = 0; i < tamanho; i++)
+                    {
+                        sup = remove_acento(nometabela[i], &diferenca);//CALCULA QUANTOS LETRAS COM ACENTUAÇÃO TEM
+                    }
+                    printf(" %-*s",quantidadeDeEspacos - diferenca, nometabela);
+                    printf("...    |\n");
+                }else
+                {
+                    int tamanho = strlen(nometabela), diferenca = 0;
+                    char sup;
+                    for (int i = 0; i < tamanho; i++)
+                    {
+                        sup = remove_acento(nometabela[i], &diferenca);//CALCULA QUANTOS LETRAS COM ACENTUAÇÃO TEM
+                    }
+                    printf(" %-*s",quantidadeDeEspacos - diferenca/2, nometabela);
+                    printf("  |\n");
+                }
+            }
         }
 
         if(*quadrodeavisos->invalido){
@@ -128,10 +144,10 @@ if(*quadrodeavisos->quadroAvisosAtivo == 0){
             printf("                             |\n");
         }
         if(tabela != NULL){
-        	if(*quadrodeavisos->tabelaCarregada){
-            	printf("                             |\n");
-        	}
-    	}
+            if(*quadrodeavisos->tabelaCarregada){
+                printf("                             |\n");
+            }
+        }
         if(*quadrodeavisos->invalido){
             printf(" tecla para continuar.       |\n");
         }
@@ -144,8 +160,8 @@ if(*quadrodeavisos->quadroAvisosAtivo == 0){
             printf(" Foi salva com sucesso!      |\n");
         }
         if(*quadrodeavisos->tabelaCarregada){
-        	if(tabela != NULL){
-            	printf(" Carregada com sucesso!      |\n");
+            if(tabela != NULL){
+                printf(" Carregada com sucesso!      |\n");
             }
         }
         if(*quadrodeavisos->invalido){
@@ -160,10 +176,10 @@ if(*quadrodeavisos->quadroAvisosAtivo == 0){
             printf("                             |\n");
         }
         if(tabela != NULL){
-        	if(*quadrodeavisos->tabelaCarregada){
-            	printf("                             |\n");
-        	}
-    	}
+            if(*quadrodeavisos->tabelaCarregada){
+                printf("                             |\n");
+            }
+        }
         if(*quadrodeavisos->invalido){
             printf("                             |\n");
         }
@@ -175,4 +191,44 @@ if(*quadrodeavisos->quadroAvisosAtivo == 0){
 (*quadrodeavisos->linhaAviso)++;
 
 
+}
+
+void interfaceCriarTabela(Tabela **lambda) {
+    int colunas, chave;
+    char nome[TAMANHO_MAX_NOME];
+
+    void criarCabecalho(){
+        printf("=======================================================================\n");
+        printf("|                         CRIAR NOVA TABELA                           |\n");
+        printf("=======================================================================\n");
+    }
+
+    criarCabecalho();
+    printf("| ");
+    printf("Qual Nome da Tabela? ");
+    scanf(" %49s", nome);
+    getchar();
+    limparTela();
+
+    criarCabecalho();
+    printf("| Nome da sua tabela: %s |\n", nome);
+    printf("| ");
+    printf("Quantas Colunas você quer na sua tabela? ");
+    scanf("%d", &colunas);
+    getchar();
+    limparTela();
+
+    criarCabecalho();
+    printf("| Nome da sua tabela: %s |\n", nome);
+    printf("| Quantidade de colunas: %d |\n\n", colunas);
+
+    if (*lambda != NULL) {
+        liberarTabela(*lambda); // Libera a tabela anterior se existir
+    }
+
+    printf("A primeira coluna é a chave primaria de tipo INTEIRO.\n");
+    printf("As próximas colunas devem ter esse formato (Ex: NomeCliente;1)\n");
+    printf("Texto = 1    Número inteiro = 2    Número com casa decimal = 3\n\n");
+
+    *lambda = construtorTabela(1, colunas, nome);
 }
